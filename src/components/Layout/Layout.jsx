de71a57321a0a870f0906669ed493f3e5982815e
MuradAsadzade <muradesedzade2004@gmail.com>
2022-11-14 23:25:37 +0400

    first commit

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+import Routers from '../../routers/Routers'
+const Layout = () => {
+  return <>
+  <Header/>
+  <Routers/>
+  <Footer/>
+  </>
+}
+
+export default Layout
